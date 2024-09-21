818a5558c13b9fa651473a8ac24ab006165b0f44
GiullianaYamaguchi <tavaresgiulliana@gmail.com>
2024-09-20 23:43:49 -0300

    :feat: adding information sent by the post to the api

--- contact/api.py
+++ contact/api.py
@@ -4,8 +4,14 @@ def api_chamar(cep):
     response  = requests.get(f'https://brasilapi.com.br/api/cep/v2/{cep}')
     
     if response.status_code == 200:
-        endereço = response.json()
-        return endereço['street']
+        dados = response.json()
+        endereço = {
+            'street' : dados.get('street', 'Não encontrado'), 
+            'city' : dados.get('city', 'Não encontrado'), 
+            'neighborhood' : dados.get('neighborhood', 'Não encontrado'), 
+            'state' : dados.get('state', 'Não encontrado')
+        }
+        return endereço
     else: 
         erro = f'Erro {response.status_code} na api'
         return erro
